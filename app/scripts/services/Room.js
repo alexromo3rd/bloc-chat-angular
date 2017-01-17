e61557af6f6173fdea36adf3d9f3503b1bbacc32
Alex Romo III <alexromo3rd@gmail.com>
2017-01-16 19:14:33 -0800

    Initial commit

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -3,6 +3,12 @@
 		var ref = firebase.database().ref().child("rooms");
 		var rooms = $firebaseArray(ref);
 
+		// rooms.$add({ $value: "4"}).then(function(ref) {
+		// 	var id = ref.key;
+		// 	console.log("added record with id " + id);
+		// 	rooms.$indexFor(id); // returns location in the array
+		// });
+
 		return {
 			all: rooms
 		};
