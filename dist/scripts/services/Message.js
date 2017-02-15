1f0f6d1de9b22c0e238eb3b1ad33105e4de17fa4
Alex Romo III <alexromo3rd@gmail.com>
2017-02-15 10:57:44 -0800

    Initial commit

--- dist/scripts/services/Message.js
+++ dist/scripts/services/Message.js
@@ -5,7 +5,6 @@
 
     return {
       getByRoomId: function(roomId) {
-        // Filter the messages by their room ID.
         return $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
       }
     };
